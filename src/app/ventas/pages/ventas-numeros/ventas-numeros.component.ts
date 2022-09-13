90949a22b16515034677fa8c28bb8963e4eae402
Huntress <quiqueze@gmail.com>
2022-09-13 04:01:30 -0500

    Se agregan ejemplos de pipes para numeros

--- src/app/ventas/pages/ventas-numeros/ventas-numeros.component.ts
+++ src/app/ventas/pages/ventas-numeros/ventas-numeros.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-ventas-numeros',
-  templateUrl: './ventas-numeros.component.html',
-  styleUrls: ['./ventas-numeros.component.css']
+    selector: 'app-ventas-numeros',
+    templateUrl: './ventas-numeros.component.html',
+    styleUrls: ['./ventas-numeros.component.css']
 })
 export class VentasNumerosComponent implements OnInit {
+    ventasNetas: number = 12435494.1610;
+    ventasBrutas: number = 22736465.5680;
+    porcentaje: number = 0.488731;
 
-  constructor() { }
+    constructor() {
+        // Do nothing
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        // Do nothing
+    }
 
 }
