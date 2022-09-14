38938537c6d274f0e3fc27dad53329a660ba782f
Huntress <quiqueze@gmail.com>
2022-09-14 00:02:20 -0500

    Se incluyen ejemplos del uso de pipes propios de angular

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { MenubarModule } from 'primeng/menubar';
+import { FieldsetModule } from 'primeng/fieldset';
+import { PanelModule } from 'primeng/panel';
 
 
 @NgModule({
@@ -10,6 +12,8 @@ import { MenubarModule } from 'primeng/menubar';
         ButtonModule,
         CardModule,
         MenubarModule,
+        FieldsetModule,
+        PanelModule
     ],
 })
 export class PrimeNgModule { }
--- src/app/ventas/pages/ventas-no-comunes/ventas-no-comunes.component.ts
+++ src/app/ventas/pages/ventas-no-comunes/ventas-no-comunes.component.ts
@@ -1,15 +1,77 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
+
 
 @Component({
-  selector: 'app-ventas-no-comunes',
-  templateUrl: './ventas-no-comunes.component.html',
-  styleUrls: ['./ventas-no-comunes.component.css']
+    selector: 'app-ventas-no-comunes',
+    templateUrl: './ventas-no-comunes.component.html',
+    styleUrls: ['./ventas-no-comunes.component.css']
 })
 export class VentasNoComunesComponent implements OnInit {
+    nombre: string = "Susana";
+    genero: string = "femenino";
+    genmap = {
+        "masculino": "invitarlo",
+        "femenino": "invitarla",
+        "other": "invitarle"
+    }
 
-  constructor() { }
+    clientes: string[] = ["Maria","Pedro","Juan","Sonso","Tarado","Menso","Zoquete"];
+    climap = {
+        "=0": "no tenemos ningun cliente esperando",
+        "=1": "tenemos un cliente esperando",
+        "=2": "tenemos dos clientes esperando",
+        "other": "tenemos # clientes en espera"
+    }
 
-  ngOnInit(): void {
-  }
+    persona = {
+        nombre: "Alguien",
+        apellido: "",
+        email: "alguien@correo.com",
+        alias: "Sutano",
+        direccion: "Algun lugar",
+        edad: 22,
+        isNew: false
+    };
+
+    heroes = [
+        {
+            nombre: "Superman",
+            vuela: true
+        },
+        {
+            nombre: "Batman",
+            vuela: false
+        },
+        {
+            nombre: "Aquaman",
+            vuela: false
+        }
+    ]
+
+    contador = interval(3000);
+
+    valor = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Termina la promesa");
+        }, 5000);
+    });
+
+    constructor() {
+        // Do nothing
+    }
+
+    ngOnInit(): void {
+        // Do nothing
+    }
+
+    cambiarPersona() {
+        this.nombre = "Pendejin";
+        this.genero = "masculino";
+    }
+
+    borrarCliente() {
+        this.clientes.pop();
+    }
 
 }
